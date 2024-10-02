2a2ac530b54de3d9b08e2db5520a2aa3505655bc
CrackedMatter <81803926+CrackedMatter@users.noreply.github.com>
2024-10-02 21:32:28 +0200

    Use libhat for pattern scanning

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <dlfcn.h>
+#include <libhat/Scanner.hpp>
+#include <link.h>
 #include <safetyhook.hpp>
+#include <span>
 #include <string>
-#include "sig.hpp"
 
 SafetyHookInline FloatOption_ctor_hook;
 
@@ -20,10 +22,26 @@ void FloatOption_ctor_old(void* self, int a, int b, int c, void* d, std::string&
 extern "C" __attribute__ ((visibility ("default"))) void mod_preinit() {}
 
 extern "C" __attribute__ ((visibility ("default"))) void mod_init() {
-    auto r = getCodeRegion(dlopen("libminecraftpe.so", 0));
+    using namespace hat::literals::signature_literals;
 
-    if (auto addr = findSig(r, "55 41 57 41 56 41 55 41 54 53 48 83 EC 48 F3 0F 11 5C 24 10"))
+    static std::span<std::byte> r;
+
+    dl_iterate_phdr([](dl_phdr_info* info, size_t, void* mc) {
+        auto h = dlopen(info->dlpi_name, RTLD_NOLOAD);
+        dlclose(h);
+        if (h == mc) {
+            for (auto& phdr: std::span{info->dlpi_phdr, info->dlpi_phnum}) {
+                if (phdr.p_type == PT_LOAD && phdr.p_flags & PF_X) {
+                    r = {reinterpret_cast<std::byte*>(info->dlpi_addr + phdr.p_vaddr), phdr.p_memsz};
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }, dlopen("libminecraftpe.so", 0));
+
+    if (auto addr = hat::find_pattern<hat::scan_alignment::X16>(r.begin(), r.end(), "55 41 57 41 56 41 55 41 54 53 48 83 EC 48 F3 0F 11 5C 24 10"_sig).get())
         FloatOption_ctor_hook = safetyhook::create_inline(addr, reinterpret_cast<void*>(FloatOption_ctor));
-    else if ((addr = findSig(r, "55 41 57 41 56 41 55 41 54 53 48 83 EC 48 0F 29 54 24 30")))
+    else if ((addr = hat::find_pattern<hat::scan_alignment::X16>(r.begin(), r.end(), "55 41 57 41 56 41 55 41 54 53 48 83 EC 48 0F 29 54 24 30"_sig).get()))
         FloatOption_ctor_hook = safetyhook::create_inline(addr, reinterpret_cast<void*>(FloatOption_ctor_old));
 }
